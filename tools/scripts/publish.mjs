71074a4cb349a19bf41e07d39374097167674e4d
Bruno Henrique de Souza <bhenriq.souza@gmail.com>
2022-12-19 14:21:05 -0300

    Fixing github action and pre-build scripts

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -8,7 +8,6 @@
  */
 import { readCachedProjectGraph } from '@nrwl/devkit'
 import { execSync } from 'child_process'
-// import { readFileSync, writeFileSync } from 'fs'
 import chalk from 'chalk'
 
 function invariant(condition, message) {
@@ -20,18 +19,6 @@ function invariant(condition, message) {
 
 // Executing publish script: node path/to/publish.mjs {name}
 const [, , name ] = process.argv
-
-// const versionIdx = process.argv.indexOf('--version')
-// const version = versionIdx > -1 ? process.argv[versionIdx + 1] : undefined
-// const tagIdx = process.argv.indexOf('--tag')
-// const tag = tagIdx > -1 ? process.argv[tagIdx + 1] : 'next'
-
-// // A simple SemVer validation to validate the version
-// const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/
-// invariant(
-//   version && validVersion.test(version),
-//   `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-// )
 
 const graph = readCachedProjectGraph()
 const project = graph.nodes[name]
@@ -48,17 +35,6 @@ invariant(
 )
 
 process.chdir(outputPath)
-
-// // Updating the version in "package.json" before publishing
-// try {
-//   const json = JSON.parse(readFileSync(`package.json`).toString())
-//   json.version = version
-//   writeFileSync(`package.json`, JSON.stringify(json, null, 2))
-// } catch (e) {
-//   console.error(
-//     chalk.bold.red(`Error reading package.json file from library build output.`)
-//   )
-// }
 
 // Execute "npm publish" to publish
 execSync(`npm publish --access=public`)
