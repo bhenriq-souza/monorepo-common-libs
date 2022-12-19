5604c867125576c80cb8be01b130c57b3bc2ce91
Bruno Henrique de Souza <bhenriq.souza@gmail.com>
2022-12-19 14:12:02 -0300

    Fixing github action and pre-build scripts

--- tools/scripts/get-package-version.mjs
+++
@@ -1,9 +0,0 @@
-import { readFileSync } from 'fs'
-import { cwd, argv } from 'process'
-
-const [, , lib] = argv
-
-const __root = cwd()
-
-const json = JSON.parse(readFileSync(`${__root}/packages/${lib}/package.json`).toString())
-console.log(json.version)
